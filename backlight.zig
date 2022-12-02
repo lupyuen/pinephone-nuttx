673c6c13d63e2683676ed0ec507b6fdb584da40c
Lee Lup Yuen <luppy@appkaki.com>
2022-12-02 13:31:22 +0800

    Adding backlight

--- backlight.zig
+++ backlight.zig
@@ -18,6 +18,8 @@
 //***************************************************************************
 
 //! PinePhone Display Backlight Driver for Apache NuttX RTOS
+//! See https://lupyuen.github.io/articles/de#appendix-display-backlight
+//! "A64 Page ???" refers to Allwinner A64 User Manual: https://linux-sunxi.org/images/b/b4/Allwinner_A64_User_Manual_V1.1.pdf
 
 /// Import the Zig Standard Library
 const std = @import("std");
@@ -39,14 +41,87 @@ const c = @cImport({
     @cInclude("stdio.h");
 });
 
-/// Enable Display Backlight at percent brightness
+// PIO Base Address (CPUx-PORT) (A64 Page 376)
+const PIO_BASE_ADDRESS = 0x01C2_0800;
+
+// PWM Base Address (CPUx-PWM?) (A64 Page 194)
+const PWM_BASE_ADDRESS = 0x01C2_1400;
+
+// R_PIO Base Address (CPUs-PORT) (A64 Page 410)
+const R_PIO_BASE_ADDRESS = 0x01F0_2C00;
+
+// R_PWM Base Address (CPUs-PWM?) (CPUs Domain, A64 Page 256)
+const R_PWM_BASE_ADDRESS = 0x01F0_3800;
+
+/// Turn on PinePhone Display Backlight.
+/// Based on https://lupyuen.github.io/articles/de#appendix-display-backlight
 pub export fn backlight_enable(
-    percent: u32
+    percent: u32  // Percent brightness
 ) void {
     debug("backlight_enable: start, percent={}", .{ percent });
     defer { debug("backlight_enable: end", .{}); }
 
     //// TODO
+
+    // Configure PL10 for PWM
+
+    // Register PL_CFG1 (Port L Configure Register 1)
+    // At R_PIO Offset 4 (A64 Page 412)
+    // Set PL10_SELECT (Bits 8 to 10) to 2 (S_PWM)
+
+    // backlight_enable: pct=0x5a
+    // 1.0 has incorrectly documented non-presence of PH10, the circuit is in fact the same as on 1.1+
+    // configure pwm: GPL(10), GPL_R_PWM
+    // sunxi_gpio_set_cfgpin: pin=0x16a, val=2
+    // sunxi_gpio_set_cfgbank: bank_offset=362, val=2
+    // clrsetbits 0x1f02c04, 0xf00, 0x200
+
+    // Disable R_PWM (Undocumented)
+
+    // Register R_PWM_CTRL_REG? (R_PWM Control Register?)
+    // At R_PWM Offset 0 (A64 Page 194)
+    // Clear 0x40: SCLK_CH0_GATING (0=mask)
+    // clrbits 0x1f03800, 0x40
+
+    // Configure R_PWM Period (Undocumented)
+
+    // Register R_PWM_CH0_PERIOD? (R_PWM Channel 0 Period Register?)
+    // At R_PWM Offset 4 (A64 Page 195)
+    // PWM_CH0_ENTIRE_CYS (Upper 16 Bits) = Period (0x4af)
+    // PWM_CH0_ENTIRE_ACT_CYS (Lower 16 Bits) = Period * Percent / 100 (0x0437)
+    // Period = 0x4af (1199)
+    // Percent = 0x5a
+
+    // 0x1f03804 = 0x4af0437
+
+    // Enable R_PWM (Undocumented)
+
+    // Register R_PWM_CTRL_REG? (R_PWM Control Register?)
+    // At R_PWM Offset 0 (A64 Page 194)
+    // 0x5f = SCLK_CH0_GATING (1=pass) + PWM_CH0_EN (1=enable) + PWM_CH0_PRESCAL (Prescalar 1)
+
+    // 0x1f03800 = 0x5f
+
+    // Configure PH10 for Output
+
+    // Register PH_CFG1 (PH Configure Register 1)
+    // At PIO Offset 0x100 (A64 Page 401)
+    // Set PH10_SELECT (Bits 8 to 10) to 1 (Output)
+
+    // enable backlight: GPH(10), 1
+    // sunxi_gpio_set_cfgpin: pin=0xea, val=1
+    // sunxi_gpio_set_cfgbank: bank_offset=234, val=1
+    // clrsetbits 0x1c20900, 0xf00, 0x100
+    // TODO: Should 0xf00 be 0x700 instead?
+
+    // Set PH10 to High
+
+    // Register PH_DATA (PH Data Register)
+    // At PIO Offset 0x10C (A64 Page 403)
+    // Set PH10 (Bit 10) to 1 (High)
+
+    // sunxi_gpio_output: pin=0xea, val=1
+    // TODO: Set Bit 10 of PH_DATA (0x1c2090c)
 }
 
 /// Modify the specified bits in a memory mapped register.
