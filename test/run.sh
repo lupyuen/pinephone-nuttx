c04f1447933665df207a42f626c726ef7a7def65
Lee Lup Yuen <luppy@appkaki.com>
2022-12-09 17:50:29 +0800

    Diff logs

--- test/run.sh
+++ test/run.sh
@@ -17,3 +17,17 @@ gcc \
 
 ## Run the test
 ./test
+
+./test >test.log
+diff \
+    --ignore-all-space \
+    expected.log test.log \
+    | grep ">" \
+    | grep -v "up_mdelay" \
+    | grep -v "TODO" \
+    | grep -v "txlen" \
+    | grep -v "*0x1ca0200" \
+    | grep -v "*0x1ca0048" \
+    | grep -v "ret=" \
+    | grep -v "pktlen=" \
+    | grep -v "hello" \
